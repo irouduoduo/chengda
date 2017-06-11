d1960e0b64ca81f563d23cdc8fc79685e368245e
irouduoduo <762033527@qq.com>
2017-06-11 21:19:07 +0800

    Create beauty.php

--- php/beauty.php
+++ php/beauty.php
@@ -1,4 +1,4 @@
 <?php 
-  $result =  file_get_contents('../json/colloction.json');
+  $result =  file_get_contents('https://github.com/irouduoduo/chengda/json/colloction.json');
   echo $result;   
 ?>
